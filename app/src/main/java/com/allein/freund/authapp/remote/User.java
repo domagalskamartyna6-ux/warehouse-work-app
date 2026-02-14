b4c225234760271e7ebb37ea8e4957934aac07d0
domagalskamartyna6-ux <domagalskamartyna6@gmail.com>
2026-02-14 19:40:11 +0100

    Update User class with new fields and methods
    Refactor User class to include status, message, and data fields.

--- app/src/main/java/com/allein/freund/authapp/remote/User.java
+++ app/src/main/java/com/allein/freund/authapp/remote/User.java
@@ -3,15 +3,34 @@ package com.allein.freund.authapp.remote;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by freund on 1/9/18.
- */
-
 public class User {
-    @SerializedName("email")
+
+    @SerializedName("status")
     @Expose
-    private String email;
-    @SerializedName("password")
+    private String status;
+
+    @SerializedName("message")
     @Expose
-    private String password;
+    private String message;
+
+    @SerializedName("data")
+    @Expose
+    private Data data;
+
+    public String getStatus() { return status; }
+    public String getMessage() { return message; }
+    public Data getData() { return data; }
+
+    public static class Data {
+        @SerializedName("id_employee")
+        @Expose
+        private int idEmployee;
+
+        @SerializedName("skey")
+        @Expose
+        private String skey;
+
+        public int getIdEmployee() { return idEmployee; }
+        public String getSkey() { return skey; }
+    }
 }
